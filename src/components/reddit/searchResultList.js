470fd305f8cf88473a560ec5eba65ced42142df1
SeatownAC <33205876+SeatownAC@users.noreply.github.com>
2018-08-06 21:13:30 -0700

    unfinished

---
+++ src/components/reddit/searchResultList.js
@@ -0,0 +1,15 @@
+Import React, { Component } from 'react';
+
+export default class SearchList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+render() {
+    return <h1>SearchList</h1>;
+}
+}
+
+
+
+export default class SearchResultList;
